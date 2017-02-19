f36f0eff7aebebba2ea209941ca9cd538a2302b4
Kent Sinclair <kentkent@Kents-MacBook-Air.local>
2017-02-18 19:29:53 -0500

    working on UI in OptionsScreen

--- GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/OptionsScreen.java
+++ GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/OptionsScreen.java
@@ -3,10 +3,20 @@ package com.github.gfsclock.gfstimeclock;
 import android.support.v4.content.pm.ActivityInfoCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OptionsScreen extends AppCompatActivity {
 
-    @Override
+    // UI Controls
+    Button clockInButton = (Button) findViewById(R.id.ClockInButton);
+    Button clockOutButton = (Button) findViewById(R.id.ClockOutButton);
+    Button breakInButton = (Button) findViewById(R.id.BreakInButton);
+    Button breakOutButton = (Button) findViewById(R.id.BreakOutButton);
+    Button lunchInButton = (Button) findViewById(R.id.LunchInButton);
+    Button lunchOutButton = (Button) findViewById(R.id.LunchOutButton);
+    Button punchHistoryButton = (Button) findViewById(R.id.PunchHistoryButton);
+    Button changeJobButton = (Button) findViewById(R.id.ChangeJobButton);
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_screen);
