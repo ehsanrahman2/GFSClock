98807845659788dcb4358aeb2822b7e2b50129be
Kent Sinclair <kent.sinclair@gmail.com>
2017-02-19 16:07:12 -0500

    nearing endof dev for s1

--- GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/OptionsScreen.java
+++ GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/OptionsScreen.java
@@ -17,6 +17,7 @@ public class OptionsScreen extends AppCompatActivity {
     Button punchHistoryButton = (Button) findViewById(R.id.PunchHistoryButton);
     Button changeJobButton = (Button) findViewById(R.id.ChangeJobButton);
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_screen);
--- GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/ScanBadge.java
+++ GFSTimeClock/app/src/main/java/com/github/gfsclock/gfstimeclock/ScanBadge.java
@@ -10,10 +10,13 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import static com.github.gfsclock.gfstimeclock.R.layout.activity_options_screen;
+
 
 public class ScanBadge extends AppCompatActivity {
 
-
+    private String barcode;
+//    boolean scan = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,30 +27,35 @@ public class ScanBadge extends AppCompatActivity {
     public void scanBadge1(View view){
         IntentIntegrator integrator = new IntentIntegrator(this);
         integrator.initiateScan();
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String scanResults, scanFormat;
+
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if(result != null) {
             if(result.getContents() == null) {
-//                Log.d("ScanBadge", "Cancelled scan");
+                Log.d("ScanBadge", "Cancelled scan");
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
-//                Log.d("ScanBadge", "Scanned");
-                scanResults = result.getContents();
-                scanFormat = result.getFormatName();
-                Toast.makeText(this, "Scanned: " + scanResults + " Format: " + scanFormat, Toast.LENGTH_LONG).show();
-
-                // call OptionsScreen activity here
-//                Intent options = new Intent(this, OptionsScreen.class);
+                Log.d("ScanBadge", "Scanned");
+                barcode = result.getContents().substring(1);
+//                scan = true;
+                Toast.makeText(this, "Scanned: " + barcode, Toast.LENGTH_LONG).show();
+//                processScan(barcode);
 //                startActivity(options, scanResults);
             }
         } else {
             // This is important, otherwise the result will not be passed to the fragment
             super.onActivityResult(requestCode, resultCode, data);
+            Toast.makeText(this, "something goofed", Toast.LENGTH_LONG).show();
         }
+    }
+    public void processScan(String barcode){
+        Intent optionsScreen = new Intent(this, OptionsScreen.class);
+        optionsScreen.putExtra(barcode, barcode);
+        startActivity(optionsScreen);
     }
 
 }
